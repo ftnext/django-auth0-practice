b4b8e4ed29ee7334fd7cf1fca4dee36f546cdff5
ftnext <takuyafjp+develop@gmail.com>
2024-06-02 16:26:18 +0900

    [feat] Implement scope validation

--- apiexample/auth0authorization/views.py
+++ apiexample/auth0authorization/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from functools import wraps
 
-# Create your views here.
+import jwt
+from django.http import JsonResponse
+
+
+def get_token_auth_header(request):
+    auth = request.META.get("HTTP_AUTHORIZATION", None)
+    parts = auth.split()
+    token = parts[1]
+    return token
+
+
+def requires_scope(required_scope):
+    def require_scope(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = get_token_auth_header(args[0])
+            decoded = jwt.decode(token, verify=False)
+            if decoded.get("scope"):
+                token_scopes = decoded["scope"].split()
+                for token_scope in token_scopes:
+                    if token_scope == required_scope:
+                        return f(*args, **kwargs)
+
+            response = JsonResponse(
+                {"message": "You don't have access to this resource"}
+            )
+            response.status_code = 403
+            return response
+
+        return decorated
+
+    return require_scope
